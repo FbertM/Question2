de86907d4965a1945a0049ffaaeeb4c989a8cf59
Filbert <s.manunggal@gmail.com>
2022-09-08 19:16:56 +0700

    Initial Awal

---
+++ Question2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+       
+        static void Main()
+        {
+            for (int i = 1; i <= 500; i++)
+            {
+                string txt = "";
+                if (i % 3 == 0)
+                    txt += "Tik";
+                if (i % 5 == 0)
+                    txt += "Tek";
+                if (i % 7 == 0)
+                    txt += "Tok";
+                if (txt == "")
+                    txt = i.ToString();
+                Console.WriteLine(txt);
+            }
+        }
+        
+    }
+}
